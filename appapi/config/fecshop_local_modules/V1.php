484b0f87879125f12f4b0b4b07270cfd0f726b31
Terry <2358269014@qq.com>
2017-10-09 09:16:42 +0800

    update

---
+++ appapi/config/fecshop_local_modules/V1.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+return [
+    'v1' => [
+        'controllerMap' =>[
+            'customer'=> 'appapi\local\local_modules\V1\controllers\CustomerController'
+        ]
+    ]
+];
---
+++ appapi/local/local_modules/V1/controllers/CustomerController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace appapi\local\local_modules\V1\controllers;
+
+use fecshop\app\appfront\modules\AppfrontController;
+use Yii;
+
+/**
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class CustomerController extends  \fecshop\app\appapi\modules\V1\controllers\CustomerController
+{
+    public function actionIndex(){
+        
+        echo 1;exit;
+    }
+    
+    
+    
+}
