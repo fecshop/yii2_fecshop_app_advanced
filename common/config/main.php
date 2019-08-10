7e3e5dae3f6c6b787ca8c83d9047dc0e0cb9f364
Terry <2358269014@qq.com>
2019-08-10 23:29:46 +0800

    composer asset 源的问题

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
     'vendorPath' => dirname(dirname(__DIR__)).'/vendor',
     'components' => [
         'urlManager' => [
